5c38e4aa6f0121986e5525fa97faa45657578bd1
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-08-09 16:20:14 -0500

    generando funciones para analisis de textos

---
+++ funciones_manejo_texto.py
@@ -0,0 +1,11 @@
+texto ="Hola buenos dias soy Mauricio Urdaneta identificado con la cédula 123456"
+
+texto= texto.split()
+
+def extraer_datos_contratante(lista,dic):
+    for i in lista:
+        print(i)
+        
+
+extraer_datos_contratante(texto)
+

adfccaf259684fa322e00f786b9aaae03d4c8a6f
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-08-22 12:40:16 -0500

    actualizando funciones para extraer el texto del pdf

--- funciones/funciones_pdf.py
+++ funciones/funciones_pdf.py
@@ -1,26 +1,7 @@
-import pytesseract
-from PyPDF2 import PdfReader,PdfWriter
-from pdf2image import convert_from_path
-from io import BytesIO
-
-archivo_original =""
-archivo_protegido =""
-clave =""
-def proteger_pdf_y_extraer_texto(archivo_original=archivo_original,archivo_protegido=archivo_protegido,clave=clave):
-    #convertir el pdf a una lista de imagenes
-    paginas =convert_from_path(archivo_original)
-    pdf_protegido = PdfWriter()
-    for num_pagina,imagen in enumerate(paginas):
-        #aplicar OCR a las imagenes
-        texto_extraido=pytesseract.image_to_string(imagen)
-        print(f"texto extraido de la pagina {num_pagina + 1}")
-        print(texto_extraido)
-        imagen_en_bytes = Bytes10()
-        imagen.save(imagen_en_bytes,format="PDF")
-        imagen_en_bytes.seek(0)
-        imagen_como_pdf =PdfReader(imagen_en_bytes)
-        pdf_protegido.add_page((imagen_como_pdf[0]))
-    pdf_protegido.encrypt(clave)
-
-    with open(archivo_protegido,"wb")as archivo_protegido:
-        pdf_protegido.write(archivo_protegido)
+import PyPDF2
+def read_pdf(document,num_page):
+    doc= open(document)
+    reader = PyPDF2.PdfReader(doc)
+    page = reader.pages[num_page]
+    text = page.extract_text()
+    return text
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __init__ import *
 
-text=proteger_pdf_y_extraer_texto('..//contratos//contrato_1.pdf')
+
 
 
 # Cargar archivo PDF

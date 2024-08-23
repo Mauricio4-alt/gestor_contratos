e3e502a0f0ff7550e1b1709ca790db877782084d
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-08-23 16:30:51 -0500

    faltó modifición

--- funciones/funciones_manejo_texto.py
+++ funciones/funciones_manejo_texto.py
@@ -3,13 +3,14 @@ from funciones_ocr import extraer_texto
 
 texto = extraer_texto("contrato_1-1")
 
-pattern= re.compile('(el contratante?)',re.IGNORECASE)
-match = pattern.search(texto)
-print(match)
+
+
 info_d_contr= []
 def extraer_datos_contratista(lis):
     datos ={'Representante':'','Nombre':'','Cedula':''}
     patter_cedula = re.compile(r"identificado con cédula (\d)")
+    
+
 
     
 

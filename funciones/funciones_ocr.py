aea4d79e7cd194aea46653847a5cea6d2ddf6e0e
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-08-23 14:21:15 -0500

    agregando funcionalidad de lectura y configuración de idiomas para leer los documentos en español

--- funciones/funciones_ocr.py
+++ funciones/funciones_ocr.py
@@ -1,4 +1,12 @@
 import cv2
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mjurdanetal\AppData\Local\Programs\Tesseract-OCR\tesseract'
 
+def extraer_texto(img):
+    img = cv2.imread(f'C://gestor_contratos//contratos_convertidos//{img}.jpeg')
+
+    text = pytesseract.image_to_string(img,lang='spa')
+    return text
+
+print('text:',extraer_texto('contrato_1-1'))

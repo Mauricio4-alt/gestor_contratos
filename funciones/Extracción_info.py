b434cfaa4f062a02eb7a88730915952c7cc35ec8
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-09-02 16:31:57 -0500

    agregando función de análisis extraer info nit

--- funciones/Extracción_info.py
+++ funciones/Extracción_info.py
@@ -1,22 +1,25 @@
 import re
-from funciones_ocr import extraer_texto
 
-def extraer_dcontratante(texto):
+def extraer_drepresentante(texto,pa_representante,pa_cedula,pa_empre,pa_nit):
     datos ={'nombre_representante':'',
             'cedula_representante':'',
             'Empresa':'',
             'Nit':''
             }
-    n_representante = re.compile(r'(?:entre los suscritos a saber )?([\w ]+)[,;.]? identificado con cédula',re.IGNORECASE)
-    c_representante=re.compile(r'(?:de ciudadanía no\. |de ciudadanía número )(\d{1,3}\.?\d{3}\.?\d{3})',re.IGNORECASE)
-    empresa =re.compile(r'(?:representante legal de |representación de)([\w\. ]+)[,;]? ',re.IGNORECASE)
-    nit =re.compile(r'\d{3}\.\d{3}\.\d{3}[ ]?\-[ ]?\d',re.IGNORECASE)
+    n_representante = re.compile(pa_representante,re.IGNORECASE)
+    c_representante=re.compile(pa_cedula,re.IGNORECASE)
+    empresa =re.compile(pa_empre,re.IGNORECASE)
+    nit =re.compile(pa_nit,re.IGNORECASE)
     
     n_representante=n_representante.search(texto)
     c_representante=c_representante.search(texto)
     empresa = empresa.search(texto)
     nit = nit.search(texto)
-    
+    #info para el contratante
+    # r'(?:entre los suscritos a saber )?([\w ]+)[,;.]? identificado con cédula'
+    #r'(?:de ciudadanía no\. |de ciudadanía número )(\d{1,3}\.?\d{3}\.?\d{3})'
+    #r'(?:representante legal de |representación de)([\w\. ]+)[,;]? ')
+    #r'\d{3}\.\d{3}\.\d{3}[ ]?\-[ ]?\d'
     if n_representante:
         datos['nombre_representante']=n_representante.group(1)
     if c_representante:
@@ -24,47 +27,34 @@ def extraer_dcontratante(texto):
     if empresa:    
         datos['Empresa']=empresa.group(1)
     if nit:
-        datos['Nit']=nit.group()
-    return datos
+        datos['Nit']=nit.group(1)
+    return datos,texto.index(datos['Nit'])+len(datos['Nit']) - 1
         
-def extraer_dcontratista(texto):
-    datos ={'nombre_representante':'',
-            'cedula_representante':'',
-            'Empresa':'',
-            'Nit':''
-            }
-    
-    
-    n_representante = re.compile(r'(?: y por la otra |[\;] e )([\w ]+)[\n\ ,] ',re.IGNORECASE)
-    
-    # c_representante=re.compile(r'(?:de ciudadanía no\. |de ciudadanía número )(\d{1,3}\.?\d{3}\.?\d{3})',re.IGNORECASE)
-    # empresa =re.compile(r'(?:representante legal de |representación de)([\w\. ]+)[,;]? ',re.IGNORECASE)
-    # nit =re.compile(r'\d{3}\.\d{3}\.\d{3}[ ]?\-[ ]?\d',re.IGNORECASE)
-    
-    n_representante=n_representante.search(texto)
-    # c_representante=c_representante.search(texto)
-    # empresa = empresa.search(texto)
-    # nit = nit.search(texto)
-    
-    
-    if n_representante:
-        datos['nombre_representante']=n_representante.group(1)
-    # if c_representante:
-    #     datos['cedula_representante']=c_representante.group(1)
-    # if empresa:    
-    #     datos['Empresa']=empresa.group(1)
-    # if nit:
-    #     datos['Nit']=nit.group()
-    return datos
-    
-    
-    
-    
-    
-            
-    
-# texto = extraer_texto("contrato_corto-1")
-texto = extraer_texto("contrato_1-1")
-contrato=extraer_dcontratista(texto)    
-print(texto)
-print(contrato)
+
+
+
+if __name__=='__main__':
+    texto ='''CONTRATO DE PRESTACIÓN DE SERVICIOS DE CONSULTORÍA CELEBRADO
+    CASALIMPIA Y SINERGY 8 LOWELLS. |
+
+    ANTONIO LÓPEZ HAZ identificado con cédula de ciudadanía No. 19265969 de
+    Bogota,actuando en calidad de Representante Legal de CASALIMPIA S.A. , sociedad
+    legalmente constituida, identificada con NIT 860.010.451-1 debidamente facultada para
+    contratar tal como consta en el Certificado de Existencia y Representación legal expedido por
+    la Cámara de Comercio de Bogotá, documento el cual se adjunta al presente contrato, quien
+    para los efectos del presente Contrato se denominará EL CONTRATANTE; e ISABEL RIOS
+    ¡identificada con cédula de ciudadanía No. 38.289.032 de Honda, en su condición de
+    Representante Legal de la sociedad SINERGY 8 LOWELLS, sociedad legalmente
+    constituida, identificada con NIT 830.078.090-1 , con capacidad para contratar en nombre de
+    su representada, que para los'efectos del presente Contrato se denominará EL
+    CONTRATISTA; EL CONTRATANTE y EL CONTRATISTA en el clausulado'que a
+    continuación se señala, individualmente considerados se denominará como la “Parte” y
+    conjuntamente como las “Partes”; hemos acordado suscribir el presente contrato mercantil.de
+    prestación de servicios de consultoría, el cual se regirá por las siguientes cláusulas y en lo no
+    previsto en ellas por las normas del derecho civil y comercial vigentes para este tipo de
+    contratos.'''
+
+    print(texto.index(' 830.078.090-1'))
+
+        
+
